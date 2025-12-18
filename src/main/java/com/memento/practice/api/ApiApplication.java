c689940769a26224215d8f6724360b0e49eac42c
jsales2 <jsales0204@gmail.com>
2025-12-17 19:03:23 -0500

    updated pom file and created first endpoint

--- src/main/java/com/memento/practice/api/ApiApplication.java
+++ src/main/java/com/memento/practice/api/ApiApplication.java
@@ -2,12 +2,20 @@ package com.memento.practice.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@GetMapping
+	public String tester() {
+		return "hey first test";
 	}
 
 }
